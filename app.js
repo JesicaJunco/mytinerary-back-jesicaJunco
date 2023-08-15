2aacebb61be06bafe0eb511228ac19c72a2d6fac
Jesica <jesibjunco@hotmail.com>
2023-08-14 23:53:33 -0300

    Add Router

--- app.js
+++ app.js
@@ -1,25 +1,18 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import indexRouter from './router/index.router.js';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(cors());
 app.use(morgan('dev'));
+app.use(cors());
 //req es el objeto solicitud
 //res es el objeto respuesta
-app.get('/', (req, res)=> {
-    res.send('Hello word')
-})
-
-app.get('/users', (req, res) =>{
-    res.json({
-        user: 'Jesica'
-    });
-})
+app.use('/api', indexRouter);
 
 app.listen(PORT, () => console.log('Server running on port' + PORT));
 
