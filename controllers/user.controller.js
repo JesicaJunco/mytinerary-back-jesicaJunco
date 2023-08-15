d533c32cff3e9c12f8964ab3be181e37cf250f82
Jesica <jesibjunco@hotmail.com>
2023-08-15 00:10:20 -0300

    Controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,11 @@
+const controller ={
+    getUsers: (req, res) =>{
+        res.json({
+            user: 'Maria'
+        });
+    },
+    postUsers: ()=>{},
+    deleteUsers: ()=>{},
+}
+
+export default controller;
