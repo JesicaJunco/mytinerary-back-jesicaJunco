5142d389e73eda20faa76541abad6965dd25f42c
Jesica <jesibjunco@hotmail.com>
2023-09-04 08:31:51 -0300

    correcion de ruta en index.router

--- router/index.router.js
+++ router/index.router.js
@@ -1,6 +1,6 @@
 import express from "express";
-import citiesRouter from "./cities.router.js";
-import usersRouter from "./users.router.js";
+import cityRouter from "./city.router.js";
+import userRouter from "./user.router.js";
 import itinerariesRouter from "./itineraries.router.js "
 import activitiesRouter from "./activities.router.js"
 
@@ -11,8 +11,8 @@ router.get("/", (request, response) => {
     console.log(request);
     response.send("Hello World");
 });
-router.use('/cities', citiesRouter);
-router.use('/users', usersRouter);
+router.use('/cities', cityRouter);
+router.use('/users', userRouter);
 router.use('/itineraries', itinerariesRouter);
 router.use('/activities', activitiesRouter);
 
